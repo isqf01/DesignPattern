692df0e3c24cc7d63eba79ed513df35990cecdc7
isqf01 <isqf02@163.com>
2023-01-30 22:21:54 +0800

    201、单例模式(饿汉式（静态代码块）)练习

--- 02Singleton/src/qf01/Singleton02.java
+++ 02Singleton/src/qf01/Singleton02.java
@@ -4,17 +4,21 @@ package qf01;
  * @author IsQf02
  * @create 2023-01-29 21:48
  */
-public class Singleton01 {
+public class Singleton02 {
     //1.将构造器私有化,外部不能new
-    private Singleton01(){
+    private Singleton02(){
 
     }
 
     // 2.在本类内部创建对象实例
-    private final static Singleton01 instance = new Singleton01();
+    private static Singleton02 instance;
+
+    static {    //将创建对象放在静态代码块中
+        instance = new Singleton02();
+    }
 
     // 3.提供一个公有的静态方法，返回实例对象
-    public static Singleton01 getInstance(){
+    public static Singleton02 getInstance(){
         return instance;
     }
 
--- 02Singleton/src/qf01/SingletonTest01.java
+++ 02Singleton/src/qf01/SingletonTest01.java
@@ -6,11 +6,20 @@ package qf01;
  */
 public class SingletonTest01 {
     public static void main(String[] args) {
+        //测试
         Singleton01 instance1 = Singleton01.getInstance();
         Singleton01 instance2 = Singleton01.getInstance();
         System.out.println(instance1 == instance2);
 
-        System.out.println("instance1.hashCode() = "+ instance1.hashCode());
-        System.out.println("instance2.hashCode() = "+ instance2.hashCode());
+//       System.out.println("instance1.hashCode() = "+ instance1.hashCode());
+//        System.out.println("instance2.hashCode() = "+ instance2.hashCode());
+
+        //测试
+        Singleton02 instance3 = Singleton02.getInstance();
+        Singleton02 instance4=  Singleton02.getInstance();
+        System.out.println(instance1 == instance2);
+
+//        System.out.println("instance3.hashCode() = "+ instance3.hashCode());
+//        System.out.println("instance4.hashCode() = "+ instance4.hashCode());
     }
 }
