d970d9f4cf0b60dbdbe9f510aa3cf5d03de3efad
isqf01 <isqf02@163.com>
2023-01-31 16:47:46 +0800

    connection test

--- 02Singleton/src/qf01/SingletonTest01.java
+++ 02Singleton/src/qf01/SingletonTest01.java
@@ -2,7 +2,7 @@ package qf01;
 
 /**
  * @author IsQf02
- * @create 2023-01-29 21:48
+ * @time 2023-01-29 21:48
  */
 public class SingletonTest01 {
     public static void main(String[] args) {
@@ -11,15 +11,15 @@ public class SingletonTest01 {
         Singleton01 instance2 = Singleton01.getInstance();
         System.out.println(instance1 == instance2);
 
-//       System.out.println("instance1.hashCode() = "+ instance1.hashCode());
-//        System.out.println("instance2.hashCode() = "+ instance2.hashCode());
+       System.out.println("instance1.hashCode() = "+ instance1.hashCode());
+        System.out.println("instance2.hashCode() = "+ instance2.hashCode());
 
         //测试
         Singleton02 instance3 = Singleton02.getInstance();
         Singleton02 instance4=  Singleton02.getInstance();
         System.out.println(instance1 == instance2);
 
-//        System.out.println("instance3.hashCode() = "+ instance3.hashCode());
-//        System.out.println("instance4.hashCode() = "+ instance4.hashCode());
+        System.out.println("instance3.hashCode() = "+ instance3.hashCode());
+        System.out.println("instance4.hashCode() = "+ instance4.hashCode());
     }
 }
